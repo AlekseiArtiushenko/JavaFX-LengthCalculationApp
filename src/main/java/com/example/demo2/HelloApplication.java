7c440ad9212b342f2d660e74b0c8a053a950275e
Aleksei Artiushenko <aleksei.artiushenko.it@gmail.com>
2023-12-21 16:06:12 +0300

    work in progress

--- src/main/java/com/example/demo2/HelloApplication.java
+++ src/main/java/com/example/demo2/HelloApplication.java
@@ -2,17 +2,35 @@ package com.example.demo2;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-
+import java.io.*;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
+        BorderPane layout = new BorderPane();
+
+        Button button = new Button("Open a file");
+        ImageView imgView = new ImageView();
+
+
+
+        layout.setTop(button);
+        layout.setCenter(imgView);
+        BorderPane.setAlignment(button, Pos.CENTER);
+
+
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 600, 600);
-        stage.setTitle("Demo app on JavaFX");
+        stage.setTitle("JavaFX FileChooser");
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/com/example/demo2/HelloController.java
+++ src/main/java/com/example/demo2/HelloController.java
@@ -1,14 +1,39 @@
 package com.example.demo2;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import java.io.File;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Button button;
+
+    @FXML
+    private ImageView imgView;
+
+    @FXML
+    void button(ActionEvent event) {
+        button.setOnAction(actionEvent -> {
+            FileChooser fileChooser = new FileChooser();
+            fileChooser.setTitle("Open a file");
+            fileChooser.setInitialDirectory(new File("C:\\Users\\Алексей\\Pictures"));
+            fileChooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("JPEG img", "*.jpg"),
+                    new FileChooser.ExtensionFilter("PNG img", "*.png"), new FileChooser.ExtensionFilter("All img", "*.jpg", "*.png"));
+            Stage stage = (Stage) button.getScene().getWindow();
+            File selectedFile = fileChooser.showOpenDialog(stage);
+            if(selectedFile != null) {
+                Image image = new Image("file:\\\\\\" + selectedFile.getPath());
+                imgView.setImage(image);
+            } else {
+                System.out.println("No file has been selected");
+            }
+        });
     }
 }
