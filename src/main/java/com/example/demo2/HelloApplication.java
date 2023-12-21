d931ac6e2748a01058dd36daee79515d45923dfc
Aleksei Artiushenko <aleksei.artiushenko.it@gmail.com>
2023-12-21 23:04:25 +0300

    work in progress

--- src/main/java/com/example/demo2/HelloApplication.java
+++ src/main/java/com/example/demo2/HelloApplication.java
@@ -2,32 +2,17 @@ package com.example.demo2;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
-import java.io.*;
+import java.io.IOException;
+
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        BorderPane layout = new BorderPane();
-
-        Button button = new Button("Open a file");
-        ImageView imgView = new ImageView();
-
-
-
-        layout.setTop(button);
-        layout.setCenter(imgView);
-        BorderPane.setAlignment(button, Pos.CENTER);
-
-
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 600, 600);
-        stage.setTitle("JavaFX FileChooser");
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Demo app on Java FX!");
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/com/example/demo2/HelloController.java
+++ src/main/java/com/example/demo2/HelloController.java
@@ -5,18 +5,40 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 public class HelloController {
 
+    List<MovableLine> lines = new ArrayList<>();
+
     @FXML
-    private Button button;
+    private AnchorPane anchorPane;
+
+    @FXML
+    void addLine(ActionEvent event) {
+        MovableLine newLine = new MovableLine(anchorPane);
+        lines.add(newLine);
+    }
+
+    @FXML
+    void removeLine(ActionEvent event) {
+        MovableLine currentLine = lines.get(0);
+        lines.remove(0);
+        currentLine.removeLineFrom(anchorPane);
+    }
+
 
     @FXML
     private ImageView imgView;
+
+    @FXML
+    private Button button;
 
     @FXML
     void button(ActionEvent event) {
